6319e076cea230b56377af647763d9f2062efa5f
Nick De Marco <nickdmrco@gmail.com>
2020-12-08 09:49:52 -0800

    commit

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (name, devoured)
+VALUES ('Single', true), 
+('Double', false), 
+('Triple', true), 
+('Number 9', false), 
+('Number 9 Large', true),
+('Number 6 Extra Dip', false),
+('Number 7', false),
+('Number 45', true),
+('Number 45 Cheese', false);
